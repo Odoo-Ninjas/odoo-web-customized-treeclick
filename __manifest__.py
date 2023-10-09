ce1ed227c344a59eb37a1ecb3fd831c5618dd679
Marc Wimmer <marc@itewimmer.de>
2023-10-09 11:10:48 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "web_customized_tree_click",
     "version": "14.0.1.0",
-    "author": "Marc Wimmer (marc@itewimmer.de)",
+    "author": "Marc Wimmer (marc@zebroo.de)",
     "depends": ["web"],
     "data": ["views/assets.xml"],
     "external_dependencies": {"python": [], "bin": []},
