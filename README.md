bb3eaa78d1c0e9ae56ea8097d7e2cfb463221f2f
Marc Wimmer <marc@itewimmer.de>
2023-10-09 11:07:04 +0200

    [14.0]

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
+# Customized Tree Click
+
+With this module you can define which python function is called at clicking a tree
+item.
+
+## How to use
+
+```
+<field name="account_move_ids" context="{'py_onclick': 'open_in_editmode'}">
+```
+
+And in python for example:
+```
+def open_in_editmode(self):
+    return {
+        'view_type': 'form',
+        'res_model': self._name,
+        'res_id': self.id,
+        'views': [(False, 'form')],
+        'type': 'ir.actions.act_window',
+        'flags': {'form': {
+            'action_buttons': True,
+            'initial_mode': 'edit',
+        }},
+        'target': 'current',
+    }
+```
+
+
 # gimera snippet
 
 ```
--- static/src/js/list_renderer.js
+++ static/src/js/list_renderer.js
@@ -6,39 +6,34 @@ odoo.define("account_followup.open_invoice_editable", function (require) {
 		_renderRow: function (record) {
 			let row = this._super(record);
 			var self = this;
-			if (record.model == "account.move") {
-				row.addClass('o_list_no_open');
-				// add click event
-				row.bind({
-					click: function (ev) {
-						debugger;
-						if (record.context.open_edit_mode) {
-							var $target = $(ev.target);
-							if ($target[0].tagName === "BUTTON") {
-								return;
-							}
-							debugger;
-							ev.preventDefault();
-							ev.stopPropagation();
-							self._rpc({
-								model: record.model,
-								method: 'open_edit_mode',
-								args: [record.data.id],
-								kwargs: {
-									'css_class': $(ev.target).prop('class'),
-								},
-							}).then(function (action) {
-								console.log(action);
-								self.do_action(action, {
-									on_close: function () {
-										self.trigger_up('reload');
-									},
-								})
-							});
+			row.addClass('o_list_no_open');
+			// add click event
+			row.bind({
+				click: function (ev) {
+					if (record.context.py_onclick) {
+						var $target = $(ev.target);
+						if ($target[0].tagName === "BUTTON") {
+							return;
 						}
+						ev.preventDefault();
+						ev.stopPropagation();
+						self._rpc({
+							model: record.model,
+							method: record.context.py_onclick,
+							args: [record.data.id],
+							kwargs: {
+							},
+						}).then(function (action) {
+							console.log(action);
+							self.do_action(action, {
+								on_close: function () {
+									self.trigger_up('reload');
+								},
+							})
+						});
 					}
-				});
-			}
+				}
+			});
 			return row
 		},
 	});
