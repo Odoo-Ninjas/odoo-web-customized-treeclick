393768fe9b8a33eed62213aeb6b6f5b4d6493483
LUNEXTLABS Tobi <tp@lunextlabs.de>
2024-08-29 10:05:11 +0200

    [17.0]

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "web_customized_tree_click",
-    "version": "16.0.1.0",
+    "version": "17.0.0.0",
     "author": "Marc Wimmer (marc@zebroo.de)",
     "depends": ["web"],
     "data": [],
